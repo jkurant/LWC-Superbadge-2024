f8173f6e57e5220b7e671e7d37b37a5e6fe213bf
jkurant <jason.kurant@gmail.com>
2024-04-27 21:19:57 -0400

    add code to boatSearchForm

--- force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
+++ force-app/main/default/lwc/boatSearchForm/boatSearchForm.js
@@ -4,11 +4,31 @@ import getBoatTypes from '@salesforce/apex/BoatDataService.getBoatTypes';
 export default class BoatSearchForm extends LightningElement {
   selectedBoatTypeId = '';
 
+  // Private
+  error = undefined;
+
+  searchOptions;
+
+  // Wire a custom Apex method
+  boatTypes({error, data}) {
+    if (data) {
+      this.searchOptions = data.map (type => {
+        return {label: type, value: type};
+      });
+      this.searchOptions.unshift ({label: 'All Types', value: ''});
+    } else if (error) {
+      this.searchOptions = undefined;
+      this.error = error;
+    }
+  }
+
   // Fires event that the search option has changed.
   // passes boatTypeId (value of this.selectedBoatTypeId) in the detail
   handleSearchOptionChange (event) {
     this.selectedBoatTypeId = event.detail.value;
-    const searchEvent = new CustomEvent ({boatTypeId: this.selectedBoatTypeId});
+    const searchEvent = new CustomEvent ('search', {
+      detail: {boatTypeId: this.selectedBoatTypeId},
+    });
     this.dispatchEvent (searchEvent);
   }
 }
