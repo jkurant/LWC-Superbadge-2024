8937aa74f11a92b31c3cfaa38177d3e77ceb3c4d
jkurant <jason.kurant@gmail.com>
2024-04-27 20:44:25 -0400

    Added boatSearch

---
+++ force-app/main/default/lwc/boatSearch/boatSearch.js
@@ -0,0 +1,31 @@
+// imports
+import {LightningElement} from 'lwc';
+import {NavigationMixin} from 'lightning/navigation';
+
+export default class BoatSearch extends LightningElement {
+  isLoading = false;
+
+  // Handles loading event
+  handleLoading () {
+    this.isLoading = true;
+  }
+
+  // Handles done loading event
+  handleDoneLoading () {
+    this.isLoading = false;
+  }
+
+  // Handles search boat event
+  // This custom event comes from the form
+  searchBoats (event) {}
+
+  createNewBoat () {
+    this[NavigationMixin.Navigate] ({
+      type: 'standard__objectPage',
+      attributes: {
+        objectApiName: 'Boat__c',
+        actionName: 'new',
+      },
+    });
+  }
+}
